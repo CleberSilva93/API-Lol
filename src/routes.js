918cb78b03901b7f03f067eaeff132b2c0e5f3f4
JvPelai <jv.pelai@gmail.com>
2020-12-07 20:26:47 -0300

    objetos com id dos campeões

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ routes.get("/invocador/:nome", invocadorController.invocador);
 routes.get("/iconePerfil/:profileIconId", function(req, res){
     res.set('Content-Type', 'image/png');
     res.sendFile(path.resolve(`src/assets/dragontail-10.24.1/10.24.1/img/profileicon/${req.params.profileIconId}.png`));
-})
+});
+
+
 
 module.exports = routes;
