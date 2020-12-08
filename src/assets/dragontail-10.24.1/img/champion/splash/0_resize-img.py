1dd6165aeb311f3c592aa19b63f5e3dcb7445885
JvPelai <jv.pelai@gmail.com>
2020-12-08 15:38:41 -0300

    resize splash mobile

---
+++ src/assets/dragontail-10.24.1/img/champion/splash/0_resize-img.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+champions = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe",
+             "AurelionSol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Chogath",
+             "Corki", "Darius", "Diana", "Draven", "DrMundo", "Ekko", "Elise", "Evelynn", "Ezreal", "FiddleSticks", "Fiora",
+             "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia",
+             "Ivern", "Janna", "JarvanIV", "Jax", "Jayce", "Jhin", "Jinx", "Kaisa", "Kalista", "Karma", "Karthus", "Kassadin",
+             "Katarina", "Kayle", "Kayn", "Kennen", "Khazix", "Kindred", "Kled", "KogMaw", "Leblanc", "LeeSin", "Leona", "Lillia",
+             "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai",  "MasterYi", "MissFortune", "MonkeyKing", "Mordekaiser",
+             "Morgana", "Nami",  "Nasus", "Nautilus",  "Neeko", "Nidalee",  "Nocturne", "Nunu", "Olaf", "Orianna",  "Ornn",
+             "Pantheon", "Poppy",  "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "RekSai",  "Renekton",  "Rengar", "Riven",
+             "Rumble", "Ryze", "Samira",  "Sejuani", "Senna", "Seraphine", "Sett",  "Shaco", "Shen", "Shyvana",
+             "Singed",  "Sion",  "Sivir", "Skarner", "Sona", "Soraka", "Swain",  "Sylas", "Syndra", "TahmKench",
+             "Taliyah", "Talon", "Taric", "Teemo",  "Thresh", "Tristana",  "Trundle", "Tryndamere", "TwistedFate", "Twitch",
+             "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Velkoz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick",
+             "Xayah", "Xerath", "XinZhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"]
+
+crop_baixo = ['Alistar','Amumu','Anivia','Chogath','FiddleSticks','Gnar','Heimerdinger','Katarina','Kayn','Khazix','KogMaw','Lucian','Malphite','Maokai','Nocturne','Nunu','Rammus','RekSai','Diana','Skarner','TahmKench','Teemo','Twitch','Udyr','Velkoz','Warwick','Zac','Ziggs']
+
+mais_crop = ['KogMaw','Malphite']
+
+for x in mais_crop:
+    image = Image.open(x+'_0.jpg')
+    box = (0, 250, 1215, 552)
+    cropped_image = image.crop(box)
+    cropped_image.save(x+'_0_mobile.jpg')
+
