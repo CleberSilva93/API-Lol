d04c7b38bb838c40991c5d2cafcbf972026996c8
JvPelai <jv.pelai@gmail.com>
2020-12-08 17:39:06 -0300

    rotas para imagens de campeão

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,20 @@ routes.get("/iconePerfil/:profileIconId", function(req, res){
     res.sendFile(path.resolve(`src/assets/dragontail-10.24.1/10.24.1/img/profileicon/${req.params.profileIconId}.png`));
 });
 
+routes.get("/iconeChampion/:champion", function(req, res){
+    res.set('Content-Type', 'image/png');
+    res.sendFile(path.resolve(`src/assets/dragontail-10.24.1/10.24.1/img/champion/${req.params.champion}.png`));
+});
+
+routes.get("/splash/desktop/:champion", function (req, res){
+    res.set('Content-Type', 'image/jpg');
+    res.sendFile(path.resolve(`src/assets/dragontail-10.24.1/img/champion/splash/${req.params.champion}_0_desktop.jpg`));
+});
+
+routes.get("/splash/mobile/:champion", function (req, res){
+    res.set('Content-Type', 'image/jpg');
+    res.sendFile(path.resolve(`src/assets/dragontail-10.24.1/img/champion/splash/${req.params.champion}_0_mobile.jpg`));
+});
 
 
 module.exports = routes;
